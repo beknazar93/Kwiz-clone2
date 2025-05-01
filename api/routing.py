9695092861bf13738d91f664d5945f8ffedfade4
Beknazar <85113284+beknazar93@users.noreply.github.com>
2025-05-02 01:25:29 +0600

    Update routing.py

--- api/routing.py
+++ api/routing.py
@@ -5,9 +5,10 @@
 # websocket_urlpatterns = [
 #     re_path(r'ws/quiz/(?P<pin>\d+)/$', consumers.QuizConsumer.as_asgi()),
 # ]
+# yourapp/routing.py
 from django.urls import re_path
-from .consumers import QuizConsumer
+from . import consumers  # consumers.py должен быть в этом же app
 
 websocket_urlpatterns = [
-    re_path(r"ws/quiz/(?P<pin>\d+)/$", QuizConsumer.as_asgi()),
+    re_path(r"ws/game/(?P<pin>\w+)/$", consumers.GameConsumer.as_asgi()),
 ]
