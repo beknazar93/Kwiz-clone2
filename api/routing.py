c0fa64c76f846686275c2366f91114f441825e2f
beknazar93 <bekishsuper@gmail.com>
2025-05-02 01:32:41 +0600

    34
    43

--- api/routing.py
+++ api/routing.py
@@ -1,14 +1,7 @@
-# # api/routing.py
-# from django.urls import re_path
-# from . import consumers
-
-# websocket_urlpatterns = [
-#     re_path(r'ws/quiz/(?P<pin>\d+)/$', consumers.QuizConsumer.as_asgi()),
-# ]
-# yourapp/routing.py
+# api/routing.py
 from django.urls import re_path
-from . import consumers  # consumers.py должен быть в этом же app
+from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/game/(?P<pin>\w+)/$", consumers.GameConsumer.as_asgi()),
+    re_path(r'ws/quiz/(?P<pin>\d+)/$', consumers.QuizConsumer.as_asgi()),
 ]
--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,35 +1,16 @@
-# import os
-# import django
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from channels.auth import AuthMiddlewareStack
-# from django.core.asgi import get_asgi_application
-# from api.routing import websocket_urlpatterns  # импорт маршрутов
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-# django.setup()
-
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter(websocket_urlpatterns)
-#     ),
-# })
-
 import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-import yourapp.routing  # поменяй на название твоего приложения
+from api.routing import websocket_urlpatterns  # импорт маршрутов
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            yourapp.routing.websocket_urlpatterns  # <== подключаем маршруты
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
