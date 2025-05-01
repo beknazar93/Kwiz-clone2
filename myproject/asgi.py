a4aa29a0e593fb422ae3828ad1efd6314bd8b46e
Beknazar <85113284+beknazar93@users.noreply.github.com>
2025-05-02 01:04:47 +0600

    Update asgi.py

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,16 +1,31 @@
-import os
-import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-from api.routing import websocket_urlpatterns  # импорт маршрутов
+# import os
+# import django
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# from django.core.asgi import get_asgi_application
+# from api.routing import websocket_urlpatterns  # импорт маршрутов
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-django.setup()
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+# django.setup()
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(websocket_urlpatterns)
+#     ),
+# })
+
+import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import api.routing
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Kwiz-clone2.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+        URLRouter(api.routing.websocket_urlpatterns)
     ),
 })
